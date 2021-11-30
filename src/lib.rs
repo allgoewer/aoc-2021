b64dac3800bb7f9edcfce0f1de568c1192af5dc9
Maik Allgöwer <maik@llgoewer.de>
2021-11-30 22:22:27 +0100

    Add collected utility function

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,9 @@ pub trait Day {
 
 /// A set of utility functions useful for handling Advent of Code quizzes
 pub mod util {
+    use std::fmt::Debug;
+    use std::str::FromStr;
+
     /// Parses a newline separated input into an [`Iterator`] over T's
     ///
     /// # Panic
@@ -23,11 +26,24 @@ pub mod util {
     /// For Advent of Code, we assume that the input is properly formatted.
     pub fn parsed<T>(input: &str) -> impl Clone + Iterator<Item = T> + '_
     where
-        T: std::str::FromStr,
-        T::Err: std::fmt::Debug,
+        T: FromStr,
+        <T as FromStr>::Err: Debug,
     {
         input
             .lines()
             .map(|l| l.trim().parse::<T>().expect("Unable to parse AOC input"))
+    }
+
+    /// Parses a newline separated input into a [`Vec<T>`]
+    ///
+    /// # Panic
+    /// This functions panics if the lines cannot be parsed.
+    /// For Advent of Code, we assume that the input is properly formatted.
+    pub fn collected<T>(input: &str) -> Vec<T>
+    where
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Debug,
+    {
+        parsed(input).collect()
     }
 }
