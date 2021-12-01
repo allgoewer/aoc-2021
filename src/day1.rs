d0e16a1f37f77092764e89cf1239152e477b67ce
Maik Allgöwer <maik@llgoewer.de>
2021-12-01 12:44:30 +0100

    Add day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,24 +1,41 @@
-//! The first day of the advent of code quiz.
-//!
-//! This is a dummy :)
-use aoc21::Day;
+//! Day 1: Sonar Sweep
+use aoc21::{util::*, Day};
 
 pub struct Today;
 
 impl Day for Today {
     fn part1(&self, input: &str) -> String {
-        input.trim().to_owned()
+        count_increases(&collected(input)).to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        input.trim().to_owned()
+        count_sliding_increases(&collected(input)).to_string()
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn ok() {
-        assert!(true);
-    }
+fn count_increases(values: &[i32]) -> usize {
+    values.windows(2).filter(|w| w[0] < w[1]).count()
+}
+
+fn count_sliding_increases(values: &[i32]) -> usize {
+    values
+        .windows(4)
+        .filter(|w| w[0] + w[1] + w[2] < w[1] + w[2] + w[3])
+        .count()
+}
+
+#[test]
+fn part1_examples() {
+    assert_eq!(
+        count_increases(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+        7
+    );
+}
+
+#[test]
+fn part2_examples() {
+    assert_eq!(
+        count_sliding_increases(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+        5
+    );
 }
