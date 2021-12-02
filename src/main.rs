049e12dc886fee54afd829c4407010ff69b24621
Maik Allgöwer <maik@llgoewer.de>
2021-12-02 07:26:10 +0100

    Add day 2

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use app::{app, help};
 
 mod app;
 mod quiz01;
+mod quiz02;
 
 /// The applications main entry point
 fn main() {
     let quizzes = all_the_quizzes![
         quiz01: "inputs/1",
+        quiz02: "inputs/2",
     ];
 
     if let Err(e) = app(&quizzes) {
---
+++ src/quiz02.rs
@@ -0,0 +1,71 @@
+//! Day 2: Dive!
+use aoc21::{util::*, Quizzer};
+use std::str::FromStr;
+
+pub struct Quiz;
+
+impl Quizzer for Quiz {
+    fn part1(&self, input: &str) -> String {
+        calc_depth_result(parsed(input)).to_string()
+    }
+
+    fn part2(&self, input: &str) -> String {
+        calc_complicated_depth_result(parsed(input)).to_string()
+    }
+}
+
+#[derive(Debug, Default)]
+struct Command(i64, i64);
+
+impl FromStr for Command {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (cmd, delta) = s
+            .split_once(' ')
+            .ok_or_else(|| anyhow::anyhow!("can't split once"))?;
+        let delta = delta.parse()?;
+
+        match (cmd, delta) {
+            ("forward", x) => Ok(Self(x, 0)),
+            ("down", y) => Ok(Self(0, y)),
+            ("up", y) => Ok(Self(0, -y)),
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn calc_depth_result(cmds: impl Iterator<Item = Command>) -> i64 {
+    let position = cmds.fold((0i64, 0i64), |(x, y), cmd| (x + cmd.0, y + cmd.1));
+    position.0 * position.1
+}
+
+fn calc_complicated_depth_result(cmds: impl Iterator<Item = Command>) -> i64 {
+    let position = cmds.fold((0i64, 0i64, 0i64), |(aim, x, y), cmd| {
+        (aim + cmd.1, x + cmd.0, y + aim * cmd.0)
+    });
+    position.1 * position.2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2";
+
+    #[test]
+    fn part1_examples() {
+        assert_eq!(calc_depth_result(parsed(EXAMPLE)), 150);
+    }
+
+    #[test]
+    fn part2_examples() {
+        assert_eq!(calc_complicated_depth_result(parsed(EXAMPLE)), 900);
+    }
+}
